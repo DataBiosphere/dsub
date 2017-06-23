d2ada94b591327e669ef43c7c74b5532f6df5a72
Gavin Bee <gavin.bee@gmail.com>
2017-06-23 12:33:04 -0400

    Lint

--- setup.py
+++ setup.py
@@ -77,8 +77,8 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'oauth2client', 'google-api-python-client', 'python-dateutil',
-        'pytz', 'pyyaml', 'tabulate'
+        'oauth2client', 'google-api-python-client', 'python-dateutil', 'pytz',
+        'pyyaml', 'tabulate'
     ],
 
     # List additional groups of dependencies here (e.g. development
