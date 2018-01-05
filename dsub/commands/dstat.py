7ffa0621c5f743ff9bda11a469d64e791dc89c6f
bryancrampton <bryancrampton@google.com>
2018-01-04 16:39:32 -0800

    Expose the job-task generator in dstat
    PiperOrigin-RevId: 180863492

--- dsub/commands/dstat.py
+++ dsub/commands/dstat.py
@@ -409,9 +409,6 @@ def dstat_job_producer(provider,
                        raw_format=False):
   """Generate jobs as lists of task dicts ready for formatting/output.
 
-  This function separates dstat logic from flag parsing and user output. Users
-  of dstat who intend to access the data programmatically should use this.
-
   Args:
     provider: an instantiated dsub provider.
     statuses: a set of status strings that eligible jobs may match.
@@ -472,6 +469,52 @@ def dstat_job_producer(provider,
       time.sleep(poll_interval)
     else:
       break
+
+
+def lookup_job_tasks(provider,
+                     statuses,
+                     user_ids=None,
+                     job_ids=None,
+                     job_names=None,
+                     task_ids=None,
+                     labels=None,
+                     create_time_min=None,
+                     create_time_max=None,
+                     max_tasks=0):
+  """Generate formatted jobs individually, in order of create-time.
+
+  Args:
+    provider: an instantiated dsub provider.
+    statuses: a set of status strings that eligible jobs may match.
+    user_ids: a set of user strings that eligible jobs may match.
+    job_ids: a set of job-id strings eligible jobs may match.
+    job_names: a set of job-name strings eligible jobs may match.
+    task_ids: a set of task-id strings eligible tasks may match.
+    labels: set of LabelParam that all tasks must match.
+    create_time_min: a timezone-aware datetime value for the earliest create
+                     time of a task, inclusive.
+    create_time_max: a timezone-aware datetime value for the most recent create
+                     time of a task, inclusive.
+    max_tasks: (int) maximum number of tasks to return per dstat job lookup.
+
+  Yields:
+    Individual task dictionaries with associated metadata
+  """
+  tasks_generator = provider.lookup_job_tasks(
+      statuses,
+      user_ids=user_ids,
+      job_ids=job_ids,
+      job_names=job_names,
+      task_ids=task_ids,
+      labels=labels,
+      create_time_min=create_time_min,
+      create_time_max=create_time_max,
+      max_tasks=max_tasks)
+
+  # Yield formatted tasks.
+  for task in tasks_generator:
+    yield _prepare_row(task, True)
+
 
 if __name__ == '__main__':
   main()
