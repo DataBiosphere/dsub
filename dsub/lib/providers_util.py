8fef56bdc520e830566801eea9172a74cb109556
pouyak <pouyak@google.com>
2018-08-13 13:00:22 -0600

    Remove trailing / from the environment variable for recursive inputs/outputs for local/google-v2 providers.
    PiperOrigin-RevId: 208519526

--- dsub/lib/providers_util.py
+++ dsub/lib/providers_util.py
@@ -61,8 +61,12 @@ def get_file_environment_variables(file_params):
   """Return a dictionary of environment variables for the user container."""
   env = {}
   for param in file_params:
-    env[param.name] = os.path.join(DATA_MOUNT_POINT,
-                                   param.docker_path) if param.value else ''
+    # We have no cases where the environment variable provided to user
+    # scripts have a trailing slash, so be sure to always strip it.
+    # The case that this is specifically handling is --input-recursive and
+    # --output-recursive variables, which are directory values.
+    env[param.name] = os.path.join(
+        DATA_MOUNT_POINT, param.docker_path.rstrip('/')) if param.value else ''
   return env
 
 
