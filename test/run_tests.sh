937fadb0d8a10c9e2ef9550fb1de9c8ef13b019d
willyn <willyn@google.com>
2024-05-22 17:36:17 +0000

    Enable e2e_io_gcs_tasks.sh
    PiperOrigin-RevId: 636217109

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -231,6 +231,7 @@ function get_test_providers() {
     e2e_input_wildcards.sh | \
     e2e_io.sh | \
     e2e_io_auto.sh | \
+    e2e_io_gcs_tasks.sh | \
     e2e_io_recursive.sh | \
     e2e_io_tasks.sh | \
     e2e_logging_content.sh | \
